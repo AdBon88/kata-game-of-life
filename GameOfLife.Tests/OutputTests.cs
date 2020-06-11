d41443c40752982eb13618e9c6c7fbd8634e7882
Adam Bonarrigo <adam.bonarrigo@myob.com>
2020-06-12 09:08:01 +1000

    all rules applying correct, all tests passing

--- GameOfLife.Tests/OutputTests.cs
+++ GameOfLife.Tests/OutputTests.cs
@@ -111,5 +111,115 @@ namespace GameOfLife.Tests
             
             Assert.Equal(expected, actual);
         }
+        
+        [Fact]
+        private void CorrectOutputWhenApplyingMultipleRulesSimultaneously()
+        {
+            var grid = new GameGrid(3,3);
+            
+            //##.
+            //##.
+            //#..
+            grid.SetCellAliveAtCoords(new[]{1,1},true);
+            grid.SetCellAliveAtCoords(new[]{2,1},true);
+            grid.SetCellAliveAtCoords(new[]{1,2},true);
+            grid.SetCellAliveAtCoords(new[]{2,2},true);
+            grid.SetCellAliveAtCoords(new[]{1,3},true);
+            
+            var game = new Game(grid);
+            game.ProgressTime();
+        
+            const string expected = "##.\n" + 
+                                    "...\n" +
+                                    "##.\n";
+        
+            var actual = Output.GridState(grid);
+            
+            Assert.Equal(expected, actual);
+        }
+        
+        [Fact]
+        private void AnotherTest()
+        {
+            var grid = new GameGrid(3,4);
+            
+            //.#.
+            //#.#
+            //#.#
+            grid.SetCellAliveAtCoords(new[]{2,1},true);
+            grid.SetCellAliveAtCoords(new[]{1,2},true);
+            grid.SetCellAliveAtCoords(new[]{3,2},true);
+            grid.SetCellAliveAtCoords(new[]{1,3},true);
+            grid.SetCellAliveAtCoords(new[]{3,3},true);
+            
+            var game = new Game(grid);
+            game.ProgressTime();
+        
+            const string expected = ".#.\n" + 
+                                    "#.#\n" +
+                                    "...\n" +
+                                    "...\n";
+        
+            var actual = Output.GridState(grid);
+            
+            Assert.Equal(expected, actual);
+        }
+        
+        [Fact]
+        private void CorrectOutputForXKCDRipJohnConwayPattern()
+        {
+            var grid = new GameGrid(7,10);
+            
+            //.......
+            //..###..
+            //..#.#..
+            //..#.#..
+            //...#...
+            //#.###..
+            //.#.#.#.
+            //...#..#
+            //..#.#..
+            //..#.#..
+            
+            grid.SetCellAliveAtCoords(new[]{3,2},true);
+            grid.SetCellAliveAtCoords(new[]{4,2},true);
+            grid.SetCellAliveAtCoords(new[]{5,2},true);
+            grid.SetCellAliveAtCoords(new[]{3,3},true);
+            grid.SetCellAliveAtCoords(new[]{5,3},true);
+            grid.SetCellAliveAtCoords(new[]{3,4},true);
+            grid.SetCellAliveAtCoords(new[]{5,4},true);
+            grid.SetCellAliveAtCoords(new[]{4,5},true);
+            grid.SetCellAliveAtCoords(new[]{1,6},true);
+            grid.SetCellAliveAtCoords(new[]{3,6},true);
+            grid.SetCellAliveAtCoords(new[]{4,6},true);
+            grid.SetCellAliveAtCoords(new[]{5,6},true);
+            grid.SetCellAliveAtCoords(new[]{2,7},true);
+            grid.SetCellAliveAtCoords(new[]{4,7},true);
+            grid.SetCellAliveAtCoords(new[]{6,7},true);
+            grid.SetCellAliveAtCoords(new[]{4,8},true);
+            grid.SetCellAliveAtCoords(new[]{7,8},true);
+            grid.SetCellAliveAtCoords(new[]{3,9},true);
+            grid.SetCellAliveAtCoords(new[]{5,9},true);
+            grid.SetCellAliveAtCoords(new[]{3,10},true);
+            grid.SetCellAliveAtCoords(new[]{5,10},true);
+            
+            var game = new Game(grid);
+            game.ProgressTime();
+        
+            const string expected = "...#...\n" + 
+                                    "..#.#..\n" +
+                                    ".##.##.\n" +
+                                    "..#.#..\n" +
+                                    ".#.....\n" +
+                                    ".#.....\n" +
+                                    ".#...#.\n" +
+                                    "...#.#.\n" +
+                                    "..#.##.\n" +
+                                    ".......\n";
+        
+            var actual = Output.GridState(grid);
+            
+            Assert.Equal(expected, actual);
+        }
     }
 }
